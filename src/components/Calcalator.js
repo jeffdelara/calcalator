9208d35154c5c417d2ce64d4cb19407503c2ca35
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-02-06 17:45:33 +0800

    Calcalator app

---
+++ src/components/Calcalator.js
@@ -0,0 +1,71 @@
+import react, { useEffect, useState } from "react";
+
+const Calcalator = () => {
+  const [price, setPrice] = useState("");
+  const [percentage, setPercentage] = useState("");
+  const [retail, setRetail] = useState(0)
+
+  useEffect(() => {
+    const p = parseFloat(price)
+    const percent = parseFloat(percentage) / 100
+    const total = (p * percent) + p
+    setRetail(total || 0)
+  }, [price, percentage])
+
+  return (
+    <>
+      <SupplierPrice price={price} setPrice={setPrice} />
+      <ProfitPercentage percentage={percentage} setPercentage={setPercentage} />
+      <Retail retail={retail} price={price} />
+    </>
+  )
+}
+
+const Retail = (props) => {
+  const {retail, price} = props
+
+  return (
+    <div className="retail">
+      <label>Profit</label>
+      <div>{retail - price}</div>
+      <label>Price</label>
+      <div>{retail}</div>
+    </div>
+  )
+}
+
+const SupplierPrice = (props) => {
+  const { setPrice, price } = props
+
+  const typingPrice = (e) => {
+    e.preventDefault();
+    const supplierPrice = e.target.value
+    setPrice(supplierPrice)
+  }
+
+  return (
+    <div>
+      <label>Supplier Price</label>
+      <input type="text" onChange={typingPrice} value={price} />
+    </div>
+  )
+}
+
+const ProfitPercentage = (props) => {
+  const { percentage, setPercentage } = props
+
+  const typingPercentage = (e) => {
+    e.preventDefault();
+    const percentage = e.target.value
+    setPercentage(percentage)
+  }
+
+  return (
+    <div>
+    <label>Percent Profit</label>
+    <input type="text" onChange={typingPercentage} value={percentage} />
+    </div>
+  )
+}
+
+export default Calcalator;
